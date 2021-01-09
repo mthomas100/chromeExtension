0f7888d22c1de469ddbae9506bc2fe7c86dd7550
mthomas100 <matthewsamuelthomas@gmail.com>
2021-01-09 14:48:56 -0500

    functionality achieved, with commented code included

--- js/background.js
+++ js/background.js
@@ -4,6 +4,31 @@ chrome.contextMenus.create({
   onclick: printCurrentTab()
 });
 
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//   const status = changeInfo.status;
+//   const url = tab.url;
+//   if (url.startsWith("https://meet.google.com") && status == "loading") {
+//       chrome.tabs.update(tabId, {url: switchUser(url)});
+//   }
+// });
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+      const tabId = details.tabId;
+      const initiator = details.initiator;
+      const url = details.url;
+      const meetId = url.match(/[a-z]{3}[-][a-z]{4}[-][a-z]{3}/gi)
+
+      console.log({initiator, url, tabId});
+      
+      if (initiator === 'https://meet.google.com' && meetId) {
+        chrome.tabs.update(tabId, {url : switchUser(`https://meet.google.com/${meetId}`)})
+        console.log(`redirect occured with initiator page of ${initiator}`);
+      }
+  },
+  {urls: ['*://meet.google.com/*'], types: ['main_frame']},
+);
+
 function printCurrentTab() {
   return function(info, tab) {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
